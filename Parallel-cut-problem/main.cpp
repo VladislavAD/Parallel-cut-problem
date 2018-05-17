81a2e860ed5ace7defffe3571500f80cedd85ba7
Владислав <vdarnos2@yandex.ru>
2018-05-17 14:46:19 +0300

    Добавлен MPI HPC Pack 2008 SDK SP2, на ноуте не запускается

--- Parallel-cut-problem/main.cpp
+++ Parallel-cut-problem/main.cpp
@@ -1,8 +1,14 @@
 #pragma once
 #include "DefaultPopulation.h"
 #include "CutSettingsHolder.h"
+#include "mpi.h"
 
-int main() {
+int main(int argc, char **argv) {
+	MPI_Init(&argc, &argv);
+	int rank, size;
+	MPI_Comm_size(MPI_COMM_WORLD, &size);
+	MPI_Comm_rank(MPI_COMM_WORLD, &rank);
+	std::cout << "The number of processes: " << size << " my number is " << rank << std::endl;
 	CutSettingsHolder * settingsHolder = new CutSettingsHolder();
 	settingsHolder->unitsSize = 30;
 	settingsHolder->newMutateUnits = 3;
@@ -13,5 +19,7 @@ int main() {
 	}
 	DefaultPopulation population = DefaultPopulation(settingsHolder);
 	while (population.Step()) {}
+	MPI_Finalize();
+	std::cin >> rank;
 	return 0;
 }
