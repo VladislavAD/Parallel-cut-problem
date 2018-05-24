1a7a8286847b9a7dd1c5a216533864ceb08bb39f
Vladislav <vdarnos2@yandex.ru>
2018-05-24 12:25:09 +0300

    Тест CutSettingsHolder передачи в другие потоки

--- Parallel-cut-problem/main.cpp
+++ Parallel-cut-problem/main.cpp
@@ -4,22 +4,28 @@
 #include "mpi.h"
 
 int main(int argc, char **argv) {
-	//MPI_Init(&argc, &argv);
-	//int rank, size;
-	//MPI_Comm_size(MPI_COMM_WORLD, &size);
-	//MPI_Comm_rank(MPI_COMM_WORLD, &rank);
+	MPI_Init(&argc, &argv);
+	int rank, size;
+	MPI_Comm_size(MPI_COMM_WORLD, &size);
+	MPI_Comm_rank(MPI_COMM_WORLD, &rank);
 	//std::cout << "The number of processes: " << size << " my number is " << rank << std::endl;
-	CutSettingsHolder * settingsHolder = new CutSettingsHolder();
-	settingsHolder->unitsSize = 30;
-	settingsHolder->newMutateUnits = 3;
-	settingsHolder->newCrossingoverUnits = 0;
-	for (int i = 0; i < 4; i++)
-	{
-		settingsHolder->figures.push_back(new Figure2D());
+	if (rank == 0) {
+		Figure2D testFigure = Figure2D(std::vector<Point>({ Point(2,2), Point(2,-2), Point(-2,2) }));
+		CutSettingsHolder testCutSettingsHolder = CutSettingsHolder();
+		testCutSettingsHolder.figures.push_back(new Figure2D(testFigure));
+		testCutSettingsHolder.figures.push_back(new Figure2D(testFigure));
+		testCutSettingsHolder.lineWidth = 2;
+		for (int i = 1; i < size; i++)
+		{
+			testCutSettingsHolder.MpiSend(i, MPI_COMM_WORLD);
+		}
 	}
-	DefaultPopulation population = DefaultPopulation(settingsHolder);
-	while (population.Step()) {}
-	//MPI_Finalize();
+	else {
+		CutSettingsHolder testCutSettingsHolder = CutSettingsHolder();
+		testCutSettingsHolder.MpiReceive(0, MPI_COMM_WORLD);
+		std::cout << "My number is " << rank << std::endl << "figure test" << std::endl << testCutSettingsHolder.figures[0]->PrintFigure();
+	}
+	MPI_Finalize();
 	//std::cin >> rank;
 	return 0;
 }
