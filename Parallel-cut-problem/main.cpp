50bc949b5a5b25ab8047fbbc4eb8ff9f2a1d3daf
Vladislav <vdarnos2@yandex.ru>
2018-05-25 23:42:33 +0300

    new CutSettingsHolder

--- Parallel-cut-problem/main.cpp
+++ Parallel-cut-problem/main.cpp
@@ -10,19 +10,19 @@ int main(int argc, char **argv) {
 	//std::cout << "The number of processes: " << size << " my number is " << rank << std::endl;
 	if (rank == 0) {
 		Figure2D testFigure = Figure2D(std::vector<Point>({ Point(2,2), Point(2,-2), Point(-2,2) }));
-		CutSettingsHolder testCutSettingsHolder = CutSettingsHolder();
-		testCutSettingsHolder.figures.push_back(new Figure2D(testFigure));
-		testCutSettingsHolder.figures.push_back(new Figure2D(testFigure));
-		testCutSettingsHolder.lineWidth = 2;
+		CutSettingsHolder * testCutSettingsHolder = new CutSettingsHolder();
+		testCutSettingsHolder->figures.push_back(new Figure2D(testFigure));
+		testCutSettingsHolder->figures.push_back(new Figure2D(testFigure));
+		testCutSettingsHolder->lineWidth = 2;
 		for (int i = 1; i < size; i++)
 		{
-			testCutSettingsHolder.MpiSend(i, MPI_COMM_WORLD);
+			testCutSettingsHolder->MpiSend(i, MPI_COMM_WORLD);
 		}
 	}
 	else {
-		CutSettingsHolder testCutSettingsHolder = CutSettingsHolder();
-		testCutSettingsHolder.MpiReceive(0, MPI_COMM_WORLD);
-		std::cout << "My number is " << rank << std::endl << "figure test" << std::endl << testCutSettingsHolder.figures[0]->PrintFigure();
+		CutSettingsHolder * testCutSettingsHolder = new CutSettingsHolder();
+		testCutSettingsHolder->MpiReceive(0, MPI_COMM_WORLD);
+		std::cout << "My number is " << rank << std::endl << "figure test" << std::endl << testCutSettingsHolder->figures[0]->PrintFigure();
 	}
 	MPI_Finalize();
 	//std::cin >> rank;
