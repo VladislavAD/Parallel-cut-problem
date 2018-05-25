263a108a5e1f61f4585b58c90542b9a9f54ad715
Vladislav <vdarnos2@yandex.ru>
2018-05-25 23:37:04 +0300

    BaseSettingsHolder абстрактный

--- Parallel-cut-problem/BaseSettingsHolder.h
+++ Parallel-cut-problem/BaseSettingsHolder.h
@@ -4,10 +4,10 @@
 
 class BaseSettingsHolder : public MpiFriendly{
 public:
-	int unitsSize = 100;
-	int maximumSteps = 1000;
-	int newMutateUnits = 10;
-	int newCrossingoverUnits = 10;
-	virtual void Initialize() {};
-	virtual BaseUnit * CreateUnit() { return nullptr; }
+	int unitsSize;
+	int maximumSteps;
+	int newMutateUnits;
+	int newCrossingoverUnits;
+	virtual void Initialize() = 0;
+	virtual BaseUnit * CreateUnit() = 0;
 };
